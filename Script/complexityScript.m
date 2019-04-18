987447254306598d3c78ce49bc9230e7148086a9
XHFkindergarten <XHFkindergarten@XHFkindtendeMBP.lan>
2019-04-18 22:38:06 +0800

    4-18

--- Script/complexityScript.m
+++ Script/complexityScript.m
@@ -1,45 +1,62 @@
-
-for a = 5:9
-    for b = 0:a-1
-        title = ['y=',num2str(a),'x+',num2str(b)]
-        word = constructWord(a,b,10000);
-        data = zeros(1,300);
-        for i=1:300
-            res = [string('')];
-            for j=1:5000
-                temp = word( j : j+i-1 );
-                if(ismember(temp,res)==0)
-                    res = [res,temp];
-                end
-            end
-            data(i) = length(res)-1;
-        end
-        data
-    end
-end
-
-
-
-
-% for m = 0:8
-%     word = constructWord(9,m);
 % 
-%     data = zeros(1,300);
-%     for i=1:300
-%         res = [string('')];
-%         for j = 1:2000
-%             temp = word( j : j+i-1 );
-%             if(ismember(temp,res)==0)
-%                 res = [res,temp];
+% for a = 1:9
+%     for b = 0:9
+%         for c = 0:9
+%             title = ['y=',num2str(a),'x^2+',num2str(b),'x+',num2str(c)]
+%             word = constructType2(a,b,c,10000);
+%             data = zeros(1,300);
+%             for i=1:300
+%                 res = [string('')];
+%                 for j=1:5000
+%                     temp = word( j : j+i-1 );
+%                     if(ismember(temp,res)==0)
+%                         res = [res,temp];
+%                     end
+%                 end
+%                 data(i) = length(res)-1;
 %             end
+%             data
 %         end
-%         data(i) = length(res)-1;
 %     end
-%     string = 'y = 9x + '
-%     m
-%     data
 % end
+% 
+% 
+% 
+% 
+% % for m = 0:8
+% %     word = constructWord(9,m);
+% % 
+% %     data = zeros(1,300);
+% %     for i=1:300
+% %         res = [string('')];
+% %         for j = 1:2000
+% %             temp = word( j : j+i-1 );
+% %             if(ismember(temp,res)==0)
+% %                 res = [res,temp];
+% %             end
+% %         end
+% %         data(i) = length(res)-1;
+% %     end
+% %     string = 'y = 9x + '
+% %     m
+% %     data
+% % end
+clear
+clc
 
-
-
-
+title = ['y=3x+1']
+word = constructType1(3,1,10000)
+data = zeros(1,20);
+for i=1:20
+    res = [string('')];
+    for j=1:5000
+        temp = word( j : j+i-1 );
+        if(ismember(temp,res)==0)
+            res = [res,temp];
+        end
+    end
+    i
+    res = res(2:length(res))
+    data(i) = length(res);
+end
+data;
